d44c16de79c061e37da3d1390c18dc986ff7248c
chrishannaby <chrishannaby@gmail.com>
2025-01-06 20:03:39 -0700

    remove migrations

--- app/drizzle/migrations/0002_fixed_master_mold.sql
+++ app/drizzle/migrations/0002_fixed_master_mold.sql
@@ -1,7 +1,10 @@
 CREATE TABLE `drawSignups` (
 	`id` integer PRIMARY KEY NOT NULL,
 	`drawId` text NOT NULL,
-	`email` text NOT NULL
+	`email` text NOT NULL,
+	`firstName` text,
+	`lastName` text
 );
 --> statement-breakpoint
 CREATE UNIQUE INDEX `drawSignups_email_unique` ON `drawSignups` (`email`);
+
